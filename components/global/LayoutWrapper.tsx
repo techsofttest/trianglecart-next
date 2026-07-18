4ac8d84a0dd3fb92e43f2094d232b17097dd8eb1
Akhil-Techsoft <techsofttest@gmail.com>
2026-07-18 10:01:57 +0530

    Fix width

--- components/global/LayoutWrapper.tsx
+++ components/global/LayoutWrapper.tsx
@@ -20,7 +20,7 @@ export default function LayoutWrapper({ children }: { children: React.ReactNode 
         <>
             <Header />
             <MobileHeader />
-            <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-4 lg:pt-8 pb-4">
+            <main className="flex-1 w-full max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 pt-4 lg:pt-8 pb-4">
                 {children}
             </main>
             <Footer />
