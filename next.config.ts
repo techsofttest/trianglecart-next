5807027e6d64d7be2dd3b2a0bae94f538cda0423
Akhil-Techsoft <techsofttest@gmail.com>
2026-07-30 10:23:33 +0530

    Fix redirect

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
@@ -26,11 +25,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-
   async redirects() {
     return [
       {
-        // 1. Match any request on the naked domain (http or https) and send to https://www
+        // Redirect naked domain to https://www
         source: '/:path*',
         has: [
           {
@@ -38,30 +36,28 @@ const nextConfig: NextConfig = {
             value: 'trianglecart.com.au',
           },
         ],
-        destination: 'https://trianglecart.com.au*',
+        destination: 'https://www.trianglecart.com.au/:path*',
         permanent: true, // 301 Permanent Redirect
       },
       {
-        // 2. Match http://yourdomain.com specifically to upgrade it to https://www
+        // Redirect http to https://www
         source: '/:path*',
         has: [
           {
             type: 'header',
             key: 'x-forwarded-proto',
-            value: 'http', // Matches insecure http traffic
+            value: 'http',
           },
           {
             type: 'host',
-            value: '://trianglecart.com.au',
+            value: 'trianglecart.com.au',
           },
         ],
-        destination: 'https://trianglecart.com.au*',
-        permanent: true, // 301 Permanent Redirect
+        destination: 'https://www.trianglecart.com.au/:path*',
+        permanent: true,
       },
     ];
   }
-
-
 };
 
 export default nextConfig;
