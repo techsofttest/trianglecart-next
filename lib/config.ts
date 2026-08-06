f8e217664770b5bd13f7ef79f49ac6e51db163b8
Akhil-Techsoft <techsofttest@gmail.com>
2026-08-06 11:37:50 +0530

    Fix ABN ACN numbers

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 export const PRIMARY_CONTACT = "+91 9876544444";
 export const SECONDARY_CONTACT = "+91 9876543210";
 export const SUPPORT_EMAIL = "shop@trianglecart.com.au";
-export const ABN_NUMBER = "ABN123456789";
-export const ACN_NUMBER = "ACN123456789";
+export const ABN_NUMBER = "14687002544";
+export const ACN_NUMBER = "687002544";
